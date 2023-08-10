3303354535d0cbdbde7a97aecc92dac0c157b69e
Avadhut More <avadhutmore1998r@gmail.com>
2023-08-10 09:51:26 +0530

    serializer for message

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from .models import Group
+from .models import Group, Message
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -21,3 +21,19 @@ class GroupDetailSerializer(GroupSerializer):
 
     class Meta(GroupSerializer.Meta):
         fields = GroupSerializer.Meta.fields + ("members",)
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    receiver_id = serializers.IntegerField(
+        write_only=True, required=False
+    )
+    group_id = serializers.IntegerField(
+        write_only=True, required=False
+    )
+    sender_id = serializers.IntegerField(
+        required=False
+    )
+
+    class Meta:
+        model = Message
+        fields = ("id", "content", "receiver_id", "group_id", "sender_id")
