7c4c70dfb34f708e4f89ec3f37ebee85362e00a2
Avadhut More <avadhutmore1998r@gmail.com>
2023-08-13 11:12:36 +0530

    added default host

--- frontend/src/actions/api-endpoint.js
+++ frontend/src/actions/api-endpoint.js
@@ -1,4 +1,4 @@
-const Host = process.env.REACT_APP_HOST;
+const Host = process.env.REACT_APP_HOST || 'http://127.0.0.1:8000/';
 
 export const group_api = `${Host}/api/group`;
 export const user_api = `${Host}/api/user`;
